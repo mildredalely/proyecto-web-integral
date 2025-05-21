efeb356cf01707db3ff44d49abc1926fa9ee5523
Mildred Alely <mildredalely07@gmail.com>
2025-05-21 09:55:02 -0600

    cambios en app.html y logs en app.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'proyecto1';
+
+  constructor(){
+    console.log('funciones de prueba');
+    console.warn("Advertencia");
+    console.log("prueba de conflictos")
+  }
 }
