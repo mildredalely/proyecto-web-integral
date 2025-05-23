8bfa81535ca22d484d1c012cc1632276e09499a0
Mildred Alely <mildredalely07@gmail.com>
2025-05-23 09:42:23 -0600

    creando modulo de autenticacion

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class LoginComponent { }
